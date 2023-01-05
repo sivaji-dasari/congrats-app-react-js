641ae748686b87d2ae6e80383b88397cc0856059
sivaji <shivajidaasari@gmail.com>
2023-01-05 13:33:15 +0000

    congrats app solution

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  // Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="name">Kiran V</h1>
+      <p className="paragraph">
+        Vishnu institute of computer Education and technology Bhimavaram.
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
